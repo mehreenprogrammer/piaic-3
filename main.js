ab76e1786d68081f6d229f55add25fc9848d8f0d
mehreenprogrammer <152857879+mehreenprogrammer@users.noreply.github.com>
2023-12-09 03:59:35 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,107 @@
+var username;
+username = "sir Mustafa";
+username = 456;
+console.log(username);
+var usernames = "sir Mustaf";
+username = "sir gulam Mustafa";
+console.log("sir Mustafa");
+console.log("taking decision");
+var budget = 40000;
+var cycle = 45000;
+if (cycle < budget) {
+    console.log("buy cycle");
+}
+else {
+    console.log("i need to build my skill to earn more");
+}
+console.log("Taking decision");
+var budget2 = 40000;
+var cycle2 = 25000;
+if (budget < cycle) {
+    console.log("i do afford to purchase cycle");
+}
+else {
+    console.log("i can afford to purchase cycle");
+}
+var single = "assign";
+var num = "3";
+var userName = "Ameen";
+console.log(num); //3
+//double==march
+console.log(num == 2 || username); //false
+// triple===march-data type 
+console.log(num === 2); //false
+var num2 = 2;
+var userame = "ameen";
+console.log(num); //3
+console.log(num == 2 && username == "ameen"); //false
+//single= assgn
+var car = "bmw";
+var carcolor = "blcak";
+if (carcolor != "red") {
+    console.log("color is not red");
+}
+//single=
+//double==
+//triple===
+//single=assign
+var car2 = "xyz";
+carcolor = "black";
+console.log(car);
+//single =
+//double==!=
+//TRIPLE===!==
+console.log("TAKING DECISION");
+var budge = 40000;
+var cycl = 40000;
+var color = "red";
+if (((color == "blue" || color == "black") && cycle <= budget) || color == "red") {
+    console.log("buy cycle");
+}
+else {
+    console.log("i need to build my skills to earn more");
+}
+console.log("taking decision");
+var budgt = 40000;
+var cyle = 40000;
+var colr = "blue";
+if (cycle <= budget) {
+    if (color == "black") {
+        console.log("buycycle");
+    }
+    else {
+        console.log("color is notmatched");
+    }
+}
+else {
+    console.log("i need to build my skills to earn more");
+}
+console.log("taking decision");
+var budegt = 40000;
+var cycle1 = 40000;
+var color2 = "black";
+if (cycle <= budget) {
+    if (color == "black") {
+        console.log("buy cycle");
+    }
+}
+else {
+    console.log("i need to build my skills to earn more");
+}
+console.log("taking decision");
+var raita = true;
+var biscuit = true;
+if (raita) {
+    console.log("buy samosa");
+}
+else if (biscuit) {
+    console.log("don't buy");
+}
+var salary = 40000;
+var monthlyexpense = 80000;
+if (salary === 40000) {
+    console.log("your have not enought money");
+}
+else {
+    console.log("you have enough money");
+}
---
+++ main.ts
@@ -0,0 +1,132 @@
+    let username:any
+    username="sir Mustafa"
+    username=456
+    console.log(username)
+
+
+    let userNames:string="sir Mustaf"
+    username="sir gulam Mustafa"
+    console.log("sir Mustafa")
+
+
+console.log("taking decision");
+let budget = 40000
+let cycle = 45000
+if(cycle< budget){
+    console.log("buy cycle")
+}
+else{
+    console.log("i need to build my skill to earn more")
+}
+console.log("Taking decision");
+let budget2= 40000
+let cycle2=25000
+if (budget<cycle){
+    console.log("i do afford to purchase cycle")
+}else{
+    console.log("i can afford to purchase cycle")
+}
+
+
+
+
+  const single = "assign"
+  let num :any ="3"
+  let userName ="Ameen"
+  console.log(num)//3
+  //double==march
+  console.log(num==2|| username)//false
+ // triple===march-data type 
+ console.log(num===2)//false
+ 
+ 
+
+
+ let num2:any=2
+ let userame="ameen"
+ console.log(num)//3
+ console.log(num==2&& username=="ameen")//false
+
+
+
+ //single= assgn
+ let car:string="bmw"
+ let carcolor:string="blcak"
+
+ if(carcolor!="red"){
+    console.log("color is not red")
+ }
+
+ //single=
+ //double==
+ //triple===
+
+
+
+
+ //single=assign
+ let car2:string="xyz"
+  carcolor ="black"
+  console.log(car)
+  //single =
+//double==!=
+//TRIPLE===!==
+
+
+
+
+console.log("TAKING DECISION")
+let budge=40000
+let cycl=40000
+let color="red"
+if(((color=="blue"||color=="black") && cycle<=budget)||color=="red"){
+    console.log("buy cycle")
+}
+else{
+    console.log("i need to build my skills to earn more")
+}
+console.log("taking decision");
+let budgt=40000
+let cyle=40000
+let colr="blue"
+if (cycle<=budget){
+    if(color=="black"){
+        console.log("buycycle")
+    }else{
+        console.log("color is notmatched")
+    }
+}
+else{
+    console.log("i need to build my skills to earn more")
+}
+console.log("taking decision");
+let budegt=40000
+let cycle1=40000
+let color2="black"
+if (cycle<=budget){
+    if (color=="black"){
+        console.log("buy cycle")
+    }
+}
+else{
+    console.log("i need to build my skills to earn more")
+}
+console.log("taking decision");
+let raita=true
+let biscuit=true
+if(raita){
+    console.log("buy samosa")
+}
+else if (biscuit){
+    console.log("don't buy")
+}
+   let salary=40000
+   let monthlyexpense=80000 
+   if (salary===40000){
+    console.log("your have not enought money")
+   }else{
+    console.log("you have enough money")
+   }
+    
+    
+
